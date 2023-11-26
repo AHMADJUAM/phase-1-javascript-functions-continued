83e2e305052e4444c69764193853a5283b0241f2
ahmad juma <juam17020@gmail.com>
2023-11-26 23:29:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// Defines saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Defines mondayWork function expression
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Defines wrapAdjective function
+  const wrapAdjective = function(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  };
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
